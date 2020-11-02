ac215bfd299a4d1a777b61a3212280c0696e6a68
Wadhwani <MOWADHWANI@DELOITTE.COM>
2020-11-02 13:25:45 +0530

    trying Commit on azure devops re

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid.component.css']
 })
 export class GridComponent implements OnInit {
-
+  userInfo:{
+    Id:Number,
+    FirstName:string,
+    LastName:string,
+    Username:string,
+    email:string,
+    age:Number
+  }
  
 
   ngOnInit(): void {
+    console.log(this.userInfo.Id);
+    
   }
 
 }
